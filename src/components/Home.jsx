a51d2cdfb5020386efa9306573ed145b580565c1
petejos80 <petejos80@gmail.com>
2019-03-07 19:20:06 -0600

    Dynamic data example

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,6 +5,7 @@ export class Home extends React.Component {
         return (
             <div>
                 <p>This is the Home Component</p>
+                <span>35 x 24 = </span> {35 * 24}
             </div>
         );
     }
