6d52ce268b5cf619d8f4993560992aa4fa72bd03
petejos80 <petejos80@gmail.com>
2019-03-07 19:30:36 -0600

    Added additional dynamic data examples

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,8 +4,13 @@ export class Home extends React.Component {
     render() {
         return (
             <div>
-                <p>This is the Home Component</p>
+                <h1>Dynamic Data in React</h1>
+
+                <h3>Example 1:</h3>
                 <span>35 x 24 = </span> {35 * 24}
+
+                <h3>Example 2:</h3>
+                { 5 === 2 ? "Yes" : "No" }
             </div>
         );
     }
